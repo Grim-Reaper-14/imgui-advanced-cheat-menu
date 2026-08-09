dfa2127f52b971898096aadf84eec5d4f5edd006
The Devil Made Me Do It <156120260+Grim-Reaper-14@users.noreply.github.com>
2026-08-09 13:01:38 -0400

    Expand Revival V2 LegitBot settings

--- AimAssist.hpp
+++ AimAssist.hpp
@@ -1,19 +1,51 @@
 #pragma once
+
 #include "imgui.h"
 #include "util/Obf.hpp"
 #include "util/Singleton.hpp"
 #include "Module.hpp"
 
 class AimAssist : public Singleton<AimAssist>, public Module {
-	friend class Singleton<AimAssist>;
+    friend class Singleton<AimAssist>;
+
 public:
-	bool* isEnemy, * isMate, * isBot, * relativeFOV, * noSnap, * noLock;
-	int* hitbox, * targetSelection, * onHold;
-	Vec3i* reactTime, * fov;
+    bool* isEnemy;
+    bool* isMate;
+    bool* isBot;
+    bool* relativeFOV;
+    bool* noSnap;
+    bool* noLock;
 
-	AimAssist();
-	void renderImGui();
+    bool* silentAim;
+    bool* aimOnKey;
+    bool* visibleCheck;
+    bool* prediction;
+    bool* latencyCompensation;
+    bool* bulletTravelTime;
 
-	void onEnable() override {}
-	void onDisable() override {}
+    bool* usePistols;
+    bool* useRifles;
+    bool* useSMGs;
+    bool* useSnipers;
+    bool* useShotguns;
+
+    int* hitbox;
+    int* targetSelection;
+    int* hitboxPriority;
+    int* onHold;
+
+    Vec3i* reactTime;
+    Vec3i* fov;
+    Vec3i* triggerDelay;
+
+    Vec3f* maxDistance;
+    Vec3f* smoothAmount;
+    Vec3f* interpolation;
+    Vec3f* randomization;
+
+    AimAssist();
+    void renderImGui();
+
+    void onEnable() override {}
+    void onDisable() override {}
 };
