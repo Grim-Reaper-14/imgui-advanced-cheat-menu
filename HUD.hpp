eb53b5f3c33ffc7f8f39df57b164564aed625fb7
The Devil Made Me Do It <156120260+Grim-Reaper-14@users.noreply.github.com>
2026-08-09 13:16:30 -0400

    Expand Misc HUD options for Revival V2

--- HUD.hpp
+++ HUD.hpp
@@ -11,22 +11,34 @@ class HUD : public Singleton<HUD>, public Module {
 public:
     int* alignML = nullptr;
     int* sortML = nullptr;
+    int* statusCorner = nullptr;
 
     ImVec4* colML = nullptr;
     ImVec4* colML_Bg = nullptr;
+    ImVec4* statusAccent = nullptr;
+    ImVec4* statusBg = nullptr;
 
     Vec3f* speedML = nullptr;
     Vec3f* offsetML = nullptr;
     Vec3f* rangeML = nullptr;
+    Vec3f* hudScale = nullptr;
+    Vec3f* hudAlpha = nullptr;
 
     bool* isML = nullptr;
     bool* isMLRainbow = nullptr;
+    bool* showWatermark = nullptr;
+    bool* showFps = nullptr;
+    bool* showClock = nullptr;
+    bool* showLuaStatus = nullptr;
+    bool* showAccentLine = nullptr;
+    bool* compactStatus = nullptr;
 
     HUD();
 
     void renderImGui();
     void render();
     void renderML();
+    void renderStatus();
 
     void onEnable() override {}
     void onDisable() override {}
